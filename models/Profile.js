c82b4b8948d2f132fc035fee8bfa1459bc38e8f1
dheeru12 <dheerajkumar88889999@gmail.com>
2020-12-11 21:29:31 +0530

    commits

---
+++ models/Profile.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+});
